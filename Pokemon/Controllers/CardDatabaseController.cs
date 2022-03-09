d983d7fa1d8287930cbfb388707f6d948b172403
konstantinos makris <kostas.makris572@gmail.com>
2022-03-09 16:44:41 +0200

    CARD DATABASE PREVIEW KAI CARD DETAILS MONO TO ARXIKO

--- Pokemon/Controllers/CardDatabaseController.cs
+++ Pokemon/Controllers/CardDatabaseController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Pokemon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,37 @@ namespace Pokemon.Controllers
 {
     public class CardDatabaseController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: CardDatabase
         public ActionResult Index()
         {
-            return View();
+            return View(db.Cards.ToList());
         }
+
+
+        public ActionResult CardDetails(int? id )
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null) 
+            {
+                return HttpNotFound();
+            }
+
+            return View(card);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
