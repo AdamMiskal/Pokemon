fdd3c43c0c406e1ab7f105f974b6be098ecdb0a2
Adam Miskal <adam.nevol@gmail.com>
2022-03-10 02:49:44 +0200

    login view ,leitourgiko koubaki kinitou k upload new logo

---
+++ Pokemon/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pokemon.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        // GET: Login
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
