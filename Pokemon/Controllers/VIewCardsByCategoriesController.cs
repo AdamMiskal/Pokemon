432f555869e1db22545af8823a7e760cc6809ced
konstantinos makris <kostas.makris572@gmail.com>
2022-04-08 19:23:04 +0300

    Card details and rarity

---
+++ Pokemon/Controllers/VIewCardsByCategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pokemon.Controllers
+{
+    public class VIewCardsByCategoriesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult AllCardsByRarity(Rarity rarity)
+        {
+
+            var cards = db.Cards.Where(x => x.Rarity == rarity).Include(x => x.Image).Include(x=>x.PokemonTypes).ToList();
+
+
+            if (cards.Count<=1)
+            {
+                return RedirectToAction("Index", "Errors", new { message = "there are not excist other with this rarity" });
+            }
+            return View(cards);
+        }
+    }
+}
