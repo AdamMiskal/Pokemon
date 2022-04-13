6a9b523600bb04b9c2c06df1fc4de2a16a979a79
chvaiou <ch.vaiou@hotmail.com>
2022-04-13 20:50:14 +0300

    EditProfile

---
+++ Pokemon/Controllers/EditProfileController.cs
@@ -0,0 +1,44 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pokemon.Controllers
+{
+    public class EditProfileController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: EditProfile
+        public ActionResult Index(string id)
+        {
+            var user = db.Users.Find(id);
+            return View(user);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
