be9db49a1d54c4c0119af2b797b5404f653c08bc
Spiros Dellis <s.dellis@outlook.com>
2022-04-19 15:55:47 +0300

    Site Statistics / Team Images

---
+++ Pokemon/Controllers/SiteStatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pokemon.Controllers
+{
+    public class SiteStatisticsController : Controller
+    {
+        // GET: SiteStatistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
